4be2e6f547e7f0705a0a550d5b67600de99a98c8
ransom <auvergnerw@gmail.com>
2013-10-17 17:28:08 +0800

    dep management with bower, modules with require.js

---
+++ app/js/main.js
@@ -0,0 +1,37 @@
+require.config({
+	paths: {
+		angular: '../../bower_components/angular/angular',
+		angularRoute: '../../bower_components/angular-route/angular-route',
+		angularMocks: '../../bower_components/angular-mocks/angular-mocks',
+		text: '../../bower_components/requirejs-text/text'
+	},
+	baseUrl: 'app/js',
+	shim: {
+		'angular' : {'exports' : 'angular'},
+		'angularRoute': ['angular'],
+		'angularMocks': {
+			deps:['angular'],
+			'exports':'angular.mock'
+		}
+	},
+	priority: [
+		"angular"
+	]
+});
+
+// hey Angular, we're bootstrapping manually!
+window.name = "NG_DEFER_BOOTSTRAP!";
+
+require( [
+	'angular',
+	'app',
+	'routes'
+], function(angular, app, routes) {
+	'use strict';
+	var $html = angular.element(document.getElementsByTagName('html')[0]);
+
+	angular.element().ready(function() {
+		$html.addClass('ng-app');
+		angular.bootstrap($html, [app['name']]);
+	});
+});
