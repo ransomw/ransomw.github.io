f45b724a039415a13f3fad2ba86b2fcb15b5de7c
ransom <auvergnerw@gmail.com>
2013-11-26 16:14:21 +0800

    code cleanup:  throw out unused functions and variables

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,16 +20,6 @@ define(['angular',
 						$scope.resume_link = 'media/resume/resume-2012.pdf';
 				}]);
 
-		function tag_to_html(tag_object) {
-				if (tag_object.tag == "url") {
-						return "<a href=\""+
-								tag_object.val+"\">"+tag_object.text+"</a>";
-				} else {
-						// TODO parse error class
-						throw new Error("unrecognized type of tag");
-				}
-		}
-
 		myAppControllers.controller('BlogCtrl', [
 				'$scope', '$http',
 				function BlogCtrl($scope, $http) {
@@ -49,24 +39,19 @@ define(['angular',
 																parser_out.metadata[j][1];
 												}
 												post['title'] = post_metadata.title;
-												var post_content = "";
 												var post_items = [];
 												for (var j = 0; j < parser_out.post.length; j++) {
 														post_items[j] = {};
 														if (typeof parser_out.post[j] == 'string') {
-																post_content += parser_out.post[j];
 																post_items[j]['type'] = 'string';
 																post_items[j]['text'] = parser_out.post[j]
 														} else {
-																post_content += tag_to_html(parser_out.post[j]);
 																post_items[j]['type'] = 'url';
 																post_items[j]['val'] = parser_out.post[j].val;
 																post_items[j]['text'] = parser_out.post[j].text;
 														}
 												}
-												post['content'] = post_content;
 												post['post_items'] = post_items;
-												post['parser_out'] = parser_out.post;
 												posts.unshift(post);
 										});
 								}
